1623be285a3f4f46480ba5e7dd9849de45ea9e4f
DABrianC <DABrianC@users.noreply.github.com>
2024-07-09 09:23:07 -0500

    BC

--- scripts/sentiments_script.R
+++ scripts/sentiments_script.R
@@ -6,8 +6,12 @@ ui <- fluidPage(
   titlePanel("Sentiment Analyzer"),
   sidebarLayout(
    sidebarPanel(fileInput(inputId = "file"
-            , label = "Upload file:"
+            , label = "Drag your file here or browse for it:"
             , multiple = FALSE)
+            , checkboxGroupInput(inputId = "column_names"
+                                 , label = "Select text columns"
+                                 , inline = FALSE
+                                 , choices = names(file))
 ),
   mainPanel(
     tabsetPanel(
@@ -21,11 +25,20 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   
   # You can access the value of the widget with input$file, e.g.
-  output$DT_table <- DT::renderDT({
-    input$file |> 
-      
-    
+  rtable <- eventReactive(input$file, {
+    read_xlsx(input$file$datapath
+              , col_names = TRUE)
   })
+  
+  output$DT_table <- DT::renderDT({
+    rtable()
+  })
+  
+  output$DT_table <- DT::renderDT({
+    df <- readxl::read_xlsx(input$file
+                      , col_names = TRUE) 
+    return(df)
+    })
   
   output$table <- renderTable({
     input$file 
